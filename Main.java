895e52dc90f80efd307f1534a013ccb72cb3ba73
TimYoder <timyoder8@gmail.com>
2016-11-08 10:43:44 -0600

    added animation

--- Main.java
+++ Main.java
@@ -17,6 +17,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.swing.JTextArea;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -47,6 +49,8 @@ public class Main extends Application {
 	 public int counter=1;
 	 public int totalcount=0;
 	 public AnchorPane anchorPane = new AnchorPane();
+
+	 public boolean anFlag=false;
 	
 	 
 	 @SuppressWarnings("static-access")
@@ -65,12 +69,38 @@ public class Main extends Application {
 	components.add(seedBtn);
 	Button makeBtn = new Button();
 	components.add(makeBtn);
+	Button anBtn= new Button();
+	components.add(anBtn);
+
+	TextField anTxt = new TextField();
+	anTxt.setPromptText("Number of time steps per second: 1");
+	anTxt.setMinWidth(50);
+	anTxt.setPrefWidth(250);
+	anTxt.setMaxWidth(400);
+	components.add(anTxt);
 	TextField stepTxt = new TextField();
 	stepTxt.setPromptText("Number of time steps : 1");
+	stepTxt.setMinWidth(50);
+	stepTxt.setPrefWidth(250);
+	stepTxt.setMaxWidth(400);
 	components.add(stepTxt);
 	TextField seedTxt = new TextField();
 	seedTxt.setPromptText("Enter random seed");
+	seedTxt.setMinWidth(50);
+	seedTxt.setPrefWidth(250);
+	seedTxt.setMaxWidth(400);
 	components.add(seedTxt);
+	final TextField runStatsTF = new TextField();
+	runStatsTF.setPromptText("This is where stats will be displayed");
+	runStatsTF.setMinWidth(50);
+	runStatsTF.setPrefWidth(250);
+	runStatsTF.setMaxWidth(400);
+	components.add(runStatsTF);
+	Button runStatsBtn = new Button();
+	components.add(runStatsBtn);
+	//Text runStatsText = new Text();
+	//runStatsText.setEditable(false);
+	//name.setPrefColumnCount(10);
 //Create grid
 	AnchorPane grid = new AnchorPane();
 	GridWorld.drawLines(grid);
@@ -78,6 +108,68 @@ public class Main extends Application {
 	anchorPane.setLeftAnchor(grid, 25.0);
     components.add(grid);
 
+    anBtn.setText("Animation");
+    anBtn.setOnAction(new EventHandler<ActionEvent>(){
+    	public void handle(ActionEvent event){
+    		int sPS = 1;
+    		try {
+    			sPS = Integer.parseInt(stepTxt.getText());
+    			if (sPS < 0) {
+    				stepTxt.clear();
+    				stepTxt.setPromptText("Not a valid input!");
+    				sPS = 0;
+    			}
+    		}
+    		catch (NumberFormatException e) {
+    			if (!stepTxt.getText().isEmpty()) {
+    				stepTxt.clear();
+    				stepTxt.setText("Not a valid input!");
+    				sPS = 0;
+    			}
+    			else {
+    				stepTxt.setPromptText("Number of time steps : 1");
+    			}
+    			
+    		}
+    		System.out.println(sPS);
+    		if(!anFlag)
+    			anFlag=true;
+    		else
+    			anFlag=false;
+    		int steps=1;
+    		int count=1;
+    		while(anFlag&&count<10&&sPS>0)
+    		{
+    			for(int i=0;i<steps;i++){
+    				Critter.worldTimeStep();
+    			}
+
+    			anchorPane.getChildren().clear();
+    			anchorPane.getChildren().addAll(components);
+    			GridWorld.drawCritters(anchorPane);
+    			   primaryStage.show();
+    			counter=1;
+    			totalcount++;
+                String s = Critter.runStats(Critter.getPopulation());
+                runStatsTF.setPromptText(s);
+    			try {
+					Thread.sleep(1000/sPS);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+    			count++;
+    		}
+    	}
+    });
+    runStatsBtn.setText("Run Stats");
+    runStatsBtn.setOnAction(new EventHandler<ActionEvent>(){
+    	public void handle(ActionEvent event){
+    		String s = Critter.runStats(Critter.getPopulation());
+    		runStatsTF.setPromptText(s);
+    	//	runStatsText.setText(s);
+    	}
+    });
 	quitBtn.setText("Quit");
 	quitBtn.setOnAction(new EventHandler<ActionEvent>() {
     	public void handle(ActionEvent event) {
@@ -95,6 +187,7 @@ public class Main extends Application {
     stepBtn.setText("Step");
     stepBtn.setOnAction(new EventHandler<ActionEvent>(){
     	public void handle(ActionEvent event){
+    		
     		anchorPane.getChildren().clear();
     		anchorPane.getChildren().addAll(components);
     		int steps = 1;
@@ -124,6 +217,8 @@ public class Main extends Application {
     		GridWorld.drawCritters(anchorPane);
     		counter=1;
             totalcount++;
+            String s = Critter.runStats(Critter.getPopulation());
+            runStatsTF.setPromptText(s);
     	}
     });
     
@@ -160,18 +255,28 @@ public class Main extends Application {
     anchorPane.getChildren().addAll(components);
 
 	GridWorld.drawCritters(anchorPane);
-    anchorPane.setBottomAnchor(quitBtn,100.0);
+    anchorPane.setBottomAnchor(quitBtn,200.0);
     anchorPane.setBottomAnchor(stepBtn,300.0);
     anchorPane.setBottomAnchor(seedBtn,500.0);
     anchorPane.setBottomAnchor(makeBtn,400.0);
-    anchorPane.setRightAnchor(quitBtn,200.0);
-    anchorPane.setRightAnchor(stepBtn,200.0);
-    anchorPane.setRightAnchor(seedBtn,200.0);
-    anchorPane.setRightAnchor(makeBtn,200.0);
+    anchorPane.setBottomAnchor(anBtn,600.0);
+    anchorPane.setRightAnchor(quitBtn,300.0);
+    anchorPane.setRightAnchor(stepBtn,300.0);
+    anchorPane.setRightAnchor(seedBtn,300.0);
+    anchorPane.setRightAnchor(makeBtn,300.0);
+    anchorPane.setRightAnchor(anBtn, 300.0);
+    anchorPane.setBottomAnchor(anTxt,600.0);
+    anchorPane.setRightAnchor(anTxt,25.0);
     anchorPane.setBottomAnchor(stepTxt,300.0);
-    anchorPane.setRightAnchor(stepTxt,35.0);
+    anchorPane.setRightAnchor(stepTxt,25.0);
     anchorPane.setBottomAnchor(seedTxt, 500.0);
-    anchorPane.setRightAnchor(seedTxt,35.0);
+    anchorPane.setRightAnchor(seedTxt,25.0);
+    anchorPane.setRightAnchor(runStatsTF, 25.0);
+    anchorPane.setBottomAnchor(runStatsTF, 350.0);
+    anchorPane.setRightAnchor(runStatsBtn,300.0);
+    anchorPane.setBottomAnchor(runStatsBtn,350.0);
+   // anchorPane.setRightAnchor(runStatsText, 200.0);
+   // anchorPane.setBottomAnchor(runStatsText, 200.0);
    primaryStage.setScene(new Scene(anchorPane, 1000, 650));
    primaryStage.show();
 
